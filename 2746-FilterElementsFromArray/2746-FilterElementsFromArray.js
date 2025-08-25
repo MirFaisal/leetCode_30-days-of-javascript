321cb7e9796b9f9fdff31a1873c5ebd6295ba8a1
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-08-26 01:22:17 +0600

    Create 2746-FilterElementsFromArray

---
+++ 2746-FilterElementsFromArray/2746-FilterElementsFromArray.js
@@ -0,0 +1,9 @@
+// Last updated: 8/26/2025, 1:22:16 AM
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    return arr.filter(fn)   
+};
