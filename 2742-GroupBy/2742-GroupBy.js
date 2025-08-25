bf139ec8fd584373f9c3aa33e81ea47aa1005184
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-08-26 02:28:19 +0600

    Create 2742-GroupBy

---
+++ 2742-GroupBy/2742-GroupBy.js
@@ -0,0 +1,19 @@
+// Last updated: 8/26/2025, 2:28:19 AM
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function (fn) {
+    return this.reduce((group, item) => {
+
+        if (!group[fn(item)]) {
+            group[fn(item)] = [];
+        }
+        group[fn(item)].push(item);
+        return group;
+    }, {})
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
