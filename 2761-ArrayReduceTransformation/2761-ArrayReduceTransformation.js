c1e93635c557a2aa6048c919bdda829b27d7a3f9
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-08-26 01:22:13 +0600

    Create 2761-ArrayReduceTransformation

---
+++ 2761-ArrayReduceTransformation/2761-ArrayReduceTransformation.js
@@ -0,0 +1,10 @@
+// Last updated: 8/26/2025, 1:22:12 AM
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    return nums.reduce(fn,init)
+};
