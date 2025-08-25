52b30e0cecbe77cb87e3c92ab02a90e4f8402f59
Mir Faisal Ahmad <mirfaisal08@gmail.com>
2025-08-26 01:31:48 +0600

    Create 2734-ArrayPrototypeLast

---
+++ 2734-ArrayPrototypeLast/2734-ArrayPrototypeLast.js
@@ -0,0 +1,15 @@
+// Last updated: 8/26/2025, 1:31:48 AM
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+    if (this.length > 0) {
+        return this[this.length - 1];
+    }
+    return -1
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
